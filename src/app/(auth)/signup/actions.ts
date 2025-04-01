2265fb19ca2e22aae83760f6f77b010f4d8db190
Jay Gurav <jaymgurav@gmail.com>
2025-04-01 11:35:33 +0530

    fix: update signup redirect URL to vercel deployed site

--- src/app/(auth)/signup/actions.ts
+++ src/app/(auth)/signup/actions.ts
@@ -7,6 +7,19 @@ import { createClient } from "@/utils/supabase/server";
 import { encodedRedirect } from "@/utils/redirect";
 
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/'
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.endsWith('/') ? url : `${url}/`
+  return url
+}
+
+
 export async function signup(formData: FormData) {
 
   const supabase = await createClient();
@@ -37,6 +50,7 @@ export async function signup(formData: FormData) {
         last_name,
         gender
       },
+      emailRedirectTo: getURL()
     },
   };
 
